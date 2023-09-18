99f5e887f9b01ea05a391cea33ac4365be82ad91
Jeremy-Belec <144268291+Jeremy-Belec@users.noreply.github.com>
2023-09-18 11:48:54 -0400

    Update Ge_on_Si_test.cmd

--- Ge_on_Si_test.cmd
+++ Ge_on_Si_test.cmd
@@ -36,23 +36,23 @@
 
 
 #Si_multiplication_layer
-(sdegeo:create-rectangle (position (* L_mult -0.5) (+ A B) 0) (position (* L_charge -0.5) (+ C (+ A B)) 0) "Silicium" "Si_multiplication_layer")
+(sdegeo:create-rectangle (position (* L_mult -0.5) (+ A B) 0) (position (* L_charge -0.5) (+ C (+ A B)) 0) "Silicon" "Si_multiplication_layer")
 
-(sdegeo:create-rectangle (position (* L_charge 0.5) (+ A B) 0) (position (* L_mult 0.5) (+ C (+ A B)) 0) "Silicium" "Si_multiplication_layer")
+(sdegeo:create-rectangle (position (* L_charge 0.5) (+ A B) 0) (position (* L_mult 0.5) (+ C (+ A B)) 0) "Silicon" "Si_multiplication_layer")
 
 (sdegeo:set-default-boolean "AB")
 
-(sdegeo:create-rectangle (position (* L_dev -0.5) (+ C (+ A B)) 0) (position (* L_dev 0.5) (+ D (+ C (+ A B))) 0) "Silicium" "Si_multiplication_layer")
+(sdegeo:create-rectangle (position (* L_dev -0.5) (+ C (+ A B)) 0) (position (* L_dev 0.5) (+ D (+ C (+ A B))) 0) "Silicon" "Si_multiplication_layer")
 
 (sdegeo:set-default-boolean "ABA")
 
 
 #Si_charge_layer
-(sdegeo:create-rectangle (position (* L_charge -0.5) (+ A B) 0) (position (* L_charge 0.5) (+ C (+ A B)) 0) "Silicium" "Si_charge_layer")
+(sdegeo:create-rectangle (position (* L_charge -0.5) (+ A B) 0) (position (* L_charge 0.5) (+ C (+ A B)) 0) "Silicon" "Si_charge_layer")
 
 
 #Si_contact_layer
-(sdegeo:create-rectangle (position (* L_dev -0.5) (+ D (+ C (+ A B))) 0) (position (* L_dev 0.5) (+ E(+ D (+ C (+ A B)))) 0) "Silicium" "Si_contact_layer")
+(sdegeo:create-rectangle (position (* L_dev -0.5) (+ D (+ C (+ A B))) 0) (position (* L_dev 0.5) (+ E(+ D (+ C (+ A B)))) 0) "Silicon" "Si_contact_layer")
 
 
 
