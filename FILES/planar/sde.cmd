cec2f487d35919af12f026896c58a4e362b2422c
Jeremy-Belec <144268291+Jeremy-Belec@users.noreply.github.com>
2023-10-24 13:30:41 -0400

    Update sde.cmd

--- FILES/planar/sde.cmd
+++ FILES/planar/sde.cmd
@@ -39,11 +39,11 @@
 
 
 #Si_multiplication_layer
-(sdegeo:create-rectangle (position (* L_mult -0.5) (+ A B) 0) (position (* L_charge -0.5) (+ D (+ C (+ A B))) 0) "Silicon" "Si_multiplication_layer")
+(sdegeo:create-rectangle (position (* L_mult -0.5) A 0) (position (* L_charge -0.5) (+ D (+ C (+ A B))) 0) "Silicon" "Si_multiplication_layer")
 
-(sdegeo:create-rectangle (position (* L_charge 0.5) (+ A B) 0) (position (* L_mult 0.5) (+ D (+ C (+ A B))) 0) "Silicon" "Si_multiplication_layer")
+(sdegeo:create-rectangle (position (* L_charge 0.5) A 0) (position (* L_mult 0.5) (+ D (+ C (+ A B))) 0) "Silicon" "Si_multiplication_layer")
 
-(sdegeo:create-rectangle (position (* L_dev -0.5) (+ C (+ A B)) 0) (position (* L_dev 0.5) (+ D (+ C (+ A B))) 0) "Silicon" "Si_multiplication_layer")
+(sdegeo:create-rectangle (position (* L_mult -0.5) (+ C (+ A B)) 0) (position (* L_mult 0.5) (+ D (+ C (+ A B))) 0) "Silicon" "Si_multiplication_layer")
 
 
 
